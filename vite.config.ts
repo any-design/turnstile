051532e111a70b876b850c59b17722ba32ba9fa4
BackRunner <dev@backrunner.top>
2023-11-12 19:05:52 +0800

    fix: add type defs

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), dts()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -16,6 +17,8 @@ export default defineConfig({
     lib: {
       entry: fileURLToPath(new URL('./src/main.ts', import.meta.url)),
       name: 'Turnstile',
+      formats: ['es', 'umd'],
+      fileName: 'turnstile',
     },
     rollupOptions: {
       external: ['vue'],
